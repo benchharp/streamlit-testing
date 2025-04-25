c06c5f2eae8852a0a85df2195d1d1a8729a929a5
benchharp <ben.m.sharpe@gmail.com>
2025-04-25 15:51:20 +0100

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -241,13 +241,12 @@ with col[2]:
 
 with col[1]:
     st.markdown('#### Total Population')
-    
-    choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
-    st.plotly_chart(choropleth, use_container_width=True)
-    
+        
     heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
     
+    choropleth = make_choropleth(df_selected_year, 'states_code', 'population', selected_color_theme)
+    st.plotly_chart(choropleth, use_container_width=True)
 
 with col[0]:
     st.markdown('#### Top States')
