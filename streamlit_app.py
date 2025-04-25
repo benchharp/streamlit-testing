fcccab9a0753af81f929b129c02fa9904eaf1186
benchharp <ben.m.sharpe@gmail.com>
2025-04-25 15:34:22 +0100

    change to .py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ import plotly.express as px
 #######################
 # Page configuration
 st.set_page_config(
-    page_title="US Population Dashboard",
+    page_title="Population Dashboard",
     page_icon="🏂",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="expanded")
 
 alt.themes.enable("dark")
